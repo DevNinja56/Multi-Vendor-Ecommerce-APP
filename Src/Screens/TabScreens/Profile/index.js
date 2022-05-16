4b6311401813ef2aa0db3d0a277e302208555a80
fakharjamil <fakhar.jameel2277@gmail.com>
2022-05-16 12:37:37 +0500

    minor changes

--- Src/Screens/TabScreens/Profile/index.js
+++ Src/Screens/TabScreens/Profile/index.js
@@ -1,30 +1,4 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import {
-  heightPercentageToDP,
-  widthPercentageToDP,
-} from "react-native-responsive-screen";
-import {
-  Ionicons,
-  AntDesign,
-  MaterialCommunityIcons,
-  MaterialIcons,
-} from "@expo/vector-icons";
-import {
-  CouponTagIcon,
-  HeartIcon,
-  HelpCenterIcon,
-  OrderIcon,
-  ProfileIcon,
-} from "../../../../assets/svg";
 import { color } from "react-native-reanimated";
 import NotAuthenticated from "./NotAuthenticated";
 import Authenticated from "./Authenticated";
